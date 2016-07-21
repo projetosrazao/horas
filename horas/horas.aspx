1109884e013f27a0b290f8013ec09e4f67812073
Pedro Henrique <Pedro Henrique@HEINEKEN>
2016-07-21 10:44:42 -0300

    :)

---
+++ horas/horas.aspx
@@ -0,0 +1,5 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="horas.aspx.cs" Inherits="horas.horas" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:DropDownList ID="DropDownList1" runat="server">
+    </asp:DropDownList>
+</asp:Content>
---
+++ horas/horas.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Odbc;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace horas
+{
+    public partial class horas : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            String connString = @"Driver={C:\\Program Files\\SQL Anywhere 16\\Bin32\\dbodbc16.dll};userid=userext;servername=envel;autostop=YES;integrated=NO;filedsn=C:\*****\*****.db.dsn;debug=NO;disablemultirowfetch=NO;commlinks='SharedMemory,TCPIP{host=192.168.1.209}';compress=NO;password=Razao@123";
+            OdbcConnection myConnection = new OdbcConnection(connString);
+            
+           
+            }
+    }
+}
